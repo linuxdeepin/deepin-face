e101e26f5dd7499bb03aaeb5ab173f32dd512f60
echengqi <echengqi@uniontech.com>
2022-08-31 13:31:59 +0800

    fix: 修复锁屏界面人脸识别提示,设备不可用问题
    为避免opencv打开摄像头偶然失败,将进行三次摄像头打开尝试
    
    Log: 人脸服务打开摄像头优化
    Bug: https://pms.uniontech.com/bug-view-156313.html
    Influence: 人脸验证录入
    Change-Id: I82faf90c6bb7a91c2e78b5de503f5bd0453d0ed6

--- workmodule.cpp
+++ workmodule.cpp
@@ -37,7 +37,16 @@ void ErollThread::run()
     bool bOpen = false;
     QList<QCameraInfo>::iterator iter = cameraInfo.begin();
     while (iter != cameraInfo.end()) {
-        bOpen = m_spCapture->open(iter->deviceName().toStdString());
+        int index = 0;
+        while (index < 3) {
+            bOpen = m_spCapture->open(iter->deviceName().toStdString());
+            if (bOpen) {
+                break;
+            }
+            index++;
+            qDebug() << "open camera " <<iter->deviceName()<<" fail "<<index;
+        }
+
         if (bOpen) {
             break;
         }
@@ -266,7 +275,16 @@ void VerifyThread::run()
     bool bOpen = false;
     QList<QCameraInfo>::iterator iter = cameraInfo.begin();
     while (iter != cameraInfo.end()) {
-        bOpen = m_spCapture->open(iter->deviceName().toStdString());
+        int index = 0;
+        while (index < 3) {
+            bOpen = m_spCapture->open(iter->deviceName().toStdString());
+            if (bOpen) {
+                break;
+            }
+            index++;
+            qDebug() << "open camera " <<iter->deviceName()<<" fail "<<index;
+        }
+
         if (bOpen) {
             break;
         }
