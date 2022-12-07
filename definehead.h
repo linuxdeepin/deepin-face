571fd4d4114d803e19b8701ec1134f062fda4b1a
Robertkill <lvpeilong@uniontech.com>
2022-12-07 10:32:35 +0800

    feat: 修复接口名称修改错误问题
    修复接口名称修改错误问题
    
    Log: 接口更变
    Task: https://pms.uniontech.com/task-view-207483.html
    Influence: 接口更变
    Change-Id: Ia6db4f5dde2e470189540ccf412f9814d676c1e7

--- definehead.h
+++ definehead.h
@@ -3,7 +3,7 @@
 
 #define SERVERNAME "org.deepin.dde.Face1"
 #define SERVERPATH "/org/deepin/dde/Face1"
-#define SERVERINTERFACE "org.deepin.dde/Face1"
+#define SERVERINTERFACE "org.deepin.dde.Face1"
 #define EMPTYCHARATYPE 0
 #define FACECHARATYPE 4
 // 轻量级人脸识别模型阈值
